ccf7ec8282aa2fb37790e1f8a23969d8d0a354df
Samantha Thorne <150044477+13Thornes@users.noreply.github.com>
2024-06-12 00:17:28 -0230

    Mostly just playing around

--- init.js
+++ init.js
@@ -2,50 +2,89 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path')
 
+const route = require('./routes.js');
+const myEmitter = require('./logEvents.js');
+
 global.DEBUG = true;
 
 const server = http.createServer((request,response) => {
     if(DEBUG) console.log('Request URL:', request.url);
+    let path = './views/';
     switch (request.url) {
         case '/':
-            console.log('Index page running')
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.end('This is the index page');
+            path += 'index.html';
+            if(DEBUG) console.log(path);
+            route.indexPage(path, response);
+            myEmitter.emit('route', path);
+            console.log('index file is working')
+            // response.write(route.indexPage);
+            // response.end('index page working');
             break;
+
         case '/about':
-            console.log('About page running')
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.end('This is the about page');
+            path += 'about.html';
+            if(DEBUG) console.log(path);
+            route.indexPage(path, response);
+            myEmitter.emit('route', path);
+            console.log('about file is working')
+            // response.write(route.aboutPage);
+            // response.end('about page working');
             break;
+
+        case '/home':
+            path += 'home.html';
+            if(DEBUG) console.log(path);
+            route.homePage(path, response);
+            myEmitter.emit('route', path);
+            console.log('home file is working')
+            break
         case '/contact':
-            console.log('Contact page running')
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.end('This is the contact page');
-            break;
+            path += 'contact.html';
+            if(DEBUG) console.log(path);
+            route.contactPage(path, response);
+            myEmitter.emit('route', path);
+            console.log('contact file is working')
+            // response.write(route.contactPage);
+            // response.end('contact page working');
+            break
         case '/subscribe':
-            console.log('Subcribe page running')
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.end('This is the subscribe page');
+            path += 'subscribe.html';
+            if(DEBUG) console.log(path);
+            route.indexPage(path, response);
+            myEmitter.emit('route', path);
+            console.log('subscribe file is working')
+            // response.write(route.subscribePage);
+            // response.end('Subscribe page working');
             break;
         case '/products':
-            console.log('Product page running')
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.end('This is the products page');
+            path += 'products.html';
+            if(DEBUG) console.log(path);
+            route.indexPage(path, response);
+            myEmitter.emit('route', path);
+            console.log('products file is working')
+            // response.write(route.productsPage);
+            // response.end('products page working');
             break;
         case '/partnership':
-            console.log('Partnership page running')
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.end('This is the partnership page');
+            path += 'partnership.html';
+            if(DEBUG) console.log(path);
+            route.indexPage(path, response);
+            myEmitter.emit('route', path);
+            console.log('partnership file is working')
+            // response.write(route.partnershipPage);
+            // response.end('partnership page working');
             break;
+            
     
         default:
+            if(DEBUG) console.log('404 Not Found');
+            myEmitter.emit('error', '404 Not Found');
             response.writeHead(404, { 'Content-Type': 'text/plain' });
             response.end('404 Not Found');
             break;
     }
+});
 
-})
-
-server.listen(3050, () => {
+server.listen(3000, () => {
     console.log('Server is running...');
   });
